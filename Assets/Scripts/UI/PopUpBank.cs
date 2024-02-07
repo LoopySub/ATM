f4cde45cf6b28364c58107bc8f071968fb39f281
LoopySub <153615396+LoopySub@users.noreply.github.com>
2024-02-07 18:03:56 +0900

    [feat]출금/입금 금액 버튼 추가
    및 잔액부족 시 에러팝업 노출 추가
    input을 받아서 입금/출금 기능 추가

--- Assets/Scripts/UI/PopUpBank.cs
+++ Assets/Scripts/UI/PopUpBank.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopUpBank : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Text userName;
+    [SerializeField] private Text balance;
+    [SerializeField] private Text cash;
+    [SerializeField] private GameObject popupError;
+
+    private void Start()
     {
-        
+        Refresh();
+    }
+    
+    public void Deposit(int money)
+    {
+        if (!GameManager.Instance.User.Deposit(money))
+        {
+            popupError.SetActive(true);
+            return;
+        }
+        Refresh();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Withdraw(int money)
     {
-        
+        if (!GameManager.Instance.User.Withdraw(money))
+        {
+            popupError.SetActive(true);
+            return;
+        }
+        Refresh();
+    }
+
+    public void CustomDeposit(InputField inputfield)
+    {
+        Deposit(int.Parse(inputfield.text));
+    }
+
+    public void CustomWithdraw(InputField inputfield)
+    {
+        Withdraw(int.Parse(inputfield.text));
+    }
+
+    private void Refresh()
+    {
+        userName.text = GameManager.Instance.User.Name;
+        balance.text = GameManager.Instance.User.Balance.ToString();
+        cash.text = GameManager.Instance.User.Cash.ToString();
     }
 }
